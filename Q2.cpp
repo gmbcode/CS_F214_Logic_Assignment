528629f2d7d9da6aba2226e68dc5ee6301f1cb4e
prathameshxpatil <f20240052@hyderabad.bits-pilani.ac.in>
2025-10-19 21:47:44 +0530

    comment fix Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -33,9 +33,9 @@ static TreeNode* buildTreeRecursive(const std::string& prefix, int& index) {
         node->left = buildTreeRecursive(prefix, index);
         node->right = buildTreeRecursive(prefix, index);
     } else if (isUnaryOperator(currentChar)) {
-        // If it's a unary operator, it has one child (we'll use the left).
+        // If it's a unary operator, it has one child (we'll use the right).
         node->left = nullptr;
-        node->right = buildTreeRecursive(prefix, index); // Right child is always null for unary ops.
+        node->right = buildTreeRecursive(prefix, index); // Left child is always null for unary ops.
     }
     // else, it's an operand (a leaf node), and its children are already nullptr.
 
