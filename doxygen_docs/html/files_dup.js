f8aaaff2e83881ebd0b9a828d72ac751c07dee38
gmbcode <gmbhacademic@gmail.com>
2025-10-21 16:20:24 +0530

    docs : Added by doxygen

---
+++ doxygen_docs/html/files_dup.js
@@ -0,0 +1,11 @@
+var files_dup =
+[
+    [ "FileUtility.h", "_file_utility_8h_source.html", null ],
+    [ "Q1.h", "_q1_8h_source.html", null ],
+    [ "Q2.h", "_q2_8h_source.html", null ],
+    [ "Q3.h", "_q3_8h_source.html", null ],
+    [ "Q4.h", "_q4_8h_source.html", null ],
+    [ "Q5.h", "_q5_8h_source.html", null ],
+    [ "Q6.h", "_q6_8h_source.html", null ],
+    [ "Q7.h", "_q7_8h_source.html", null ]
+];
---
+++ doxygen_docs/html/navtreeindex0.js
@@ -0,0 +1,17 @@
+var NAVTREEINDEX0 =
+{
+"_file_utility_8h_source.html":[1,0,0],
+"_q1_8h_source.html":[1,0,1],
+"_q2_8h_source.html":[1,0,2],
+"_q3_8h_source.html":[1,0,3],
+"_q4_8h_source.html":[1,0,4],
+"_q5_8h_source.html":[1,0,5],
+"_q6_8h_source.html":[1,0,6],
+"_q7_8h_source.html":[1,0,7],
+"annotated.html":[0,0],
+"classes.html":[0,1],
+"files.html":[1,0],
+"index.html":[],
+"pages.html":[],
+"struct_tree_node.html":[0,0,0]
+};
